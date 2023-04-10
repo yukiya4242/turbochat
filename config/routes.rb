5ce7bd3c224ce0d95c5cad4e3bacbbe93eafceba
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-04-10 01:37:50 +0000

    none

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :rooms
+  root 'pages#home'
+
+
+  devise_for :users
+
+  devise_scope :user do
+    get 'users', to: 'devise/sessions#new'
+  end
+  get 'user/:id', to: 'users#show', as: 'user'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
